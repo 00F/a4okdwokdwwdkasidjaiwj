9cadf2bc73b63ab27668c8f04efcaafe9c1a27a7
` 0ff <39999036+00F@users.noreply.github.com>
2018-12-09 14:45:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('Ready.');
-    var words = ["word","word"];
+    var words = ["الله الله ربي لا اشرك بربي شيئاً","لا اله الا الله محمد رسول الله","اللهم صل على سيدنا محمد وعلى آله وصحبه وسلم تسليماً كثيرا"];
     setInterval(() => {
         client.channels.find(c => c.id == '485710163966296064').send(words[Math.floor(Math.random() * words.length)]);
     }, 200000);
