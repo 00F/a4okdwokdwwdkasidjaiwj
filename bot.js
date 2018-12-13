5fc531a1ee2282166f33967b54ce411892f3dc77
` 0ff <39999036+00F@users.noreply.github.com>
2018-12-13 23:00:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,9 @@ client.on('ready', () => {
     console.log('Ready.');
     var words = ["الله الله ربي لا اشرك بربي شيئاً","لا اله الا الله محمد رسول الله","اللهم صل على سيدنا محمد وعلى آله وصحبه وسلم تسليماً كثيرا"];
     setInterval(() => {
-        client.channels.find(c => c.id == '485710163966296064').send('ff').then(message => {message.delete(200000)});
-    }, 3000000);
-    message.delete(200000);
+        client.channels.find(c => c.id == '485710163966296064').send('ff').then(message => {message.delete(5000)});
+    }, 1000);
+    message.delete(5000);
 });
 
 client.login(process.env.BT);
